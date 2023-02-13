99e9c3038808d77534682d99713dcac0a9005bce
YOUR-USERNAME <dan.stacy@alltech.com>
2023-02-13 14:29:12 -0500

    created header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,20 @@
+// The layout component is used to wrap all pages in the app. This is where you put components that are common to all pages, such as a header, footer, or navigation menu.
+import Header from '@/components/Header'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      {/* Header */}
+      <Header />
+
+      {/* Navbar */}
+
+      {/* SearchBox */}
+
+        {children}
+      </body>
     </html>
   )
 }
